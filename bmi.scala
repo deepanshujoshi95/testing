70b9557b6433f24c7d13910dfb1ba00de945f212
deepanshujoshi95 <40980968+deepanshujoshi95@users.noreply.github.com>
2024-09-12 11:04:35 +0530

    Update bmi.scala

--- bmi.scala
+++ bmi.scala
@@ -1,28 +1,44 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.expressions.Window
-object demo2 extends App {
-  val spark = SparkSession
-    .builder()
-    .master(master = "local")
-    .appName(name="salary").getOrCreate()
-  import spark.implicits._
-  val dataDF=Seq((1, "a", "HR", 100),
-    (2, "a", "sales", 200),
-    (3, "b", "IT", 250),
-    (4, "c", "HR", 150),
-    (5, "d", "IT", 275),
-    (6, "d", "IT", 350),
-    (7, "d", "HR", 400),
-    (8, "e", "HR", 175),
-    (9, "f", "sales", 500),
-    (10, "g", "sales", 450)).toDF(colNames = "ID","Name","Department","Salary")
-  dataDF.show()
 
-  val windowSpec = Window.partitionBy("Department").orderBy($"Salary".desc)
-  val outdf = dataDF.withColumn("rank",dense_rank().over(windowSpec))
-    .where($"rank" === 2)
-    .select("Department","Salary")
+object demo11 {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("merger")
+      .master("local[*]")
+      .getOrCreate()
+    val dbUrl = "jdbc:mysql://localhost:3306/u4"
+    val dbTable = "employee"
+    val dbUser = "root"
+    val salaryDF = spark.read
+      .format("jdbc")
+      .option("url", dbUrl)
+      .option("dbtable", dbTable)
+      .option("user", dbUser)
+      //      .option("password", dbPassword)
+      .load()
 
-  outdf.show()
+    import spark.implicits._
+    val csvdf = spark.read.option("header", true).csv("C:/Users/DEEJOSHI/desktop/scala_usecase/use12.csv")
+    val jsondf = spark.read.option("multiline", true).json("C:/Users/DEEJOSHI/desktop/scala_usecase/jsoncase2.json")
+    val employeeDepartmentDF = csvdf
+      .join(jsondf, "department_id")
+    val employeeDepartmentSalaryDF = employeeDepartmentDF
+      .join(salaryDF, "employee_id")
+    val averageSalaryPerDepartment = employeeDepartmentSalaryDF
+      .groupBy("department_id", "department_name")
+      .agg(avg("salary").as("average_salary"))
+    val highestAvgSalaryDept = averageSalaryPerDepartment
+      .orderBy(desc("average_salary"))
+      .first()
+    val orderedAverageSalaryPerDepartment = averageSalaryPerDepartment
+      .orderBy(desc("average_salary"))
+    println("Department ID, Department Name, Average Salary")
+    orderedAverageSalaryPerDepartment.foreach { row =>
+      val departmentId = row.getAs[String]("department_id")
+      val departmentName = row.getAs[String]("department_name")
+      val averageSalary = row.getAs[Double]("average_salary")
+      println(s"$departmentId, $departmentName, $averageSalary")
+    }
+  }
 }
